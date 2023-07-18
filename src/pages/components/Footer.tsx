01a5610137e5ef5b1bc1f455a863612fdeb06f81
Ayush Singh <asayushsingh80@gmail.com>
2023-07-19 01:55:54 +0530

    Made rough Footer layout

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer className="none md:block md:h-[10%] max-w-[1550px] mx-auto bg-primary-500">
-      hello
+      <div className="flex flex-row justify-stretch items-stretch w-full h-full">
+        <div className="bg-orange-500 p-5 basis-1/2">Contact Us</div>
+        <div className="bg-yellow-300 p-5 basis-1/2">About Us</div>
+      </div>
     </footer>
   );
 }
