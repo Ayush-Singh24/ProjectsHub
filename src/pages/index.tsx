a16c563a2ba6e090523018484f7bae43c538395b
Ayush Singh <asayushsingh80@gmail.com>
2023-07-18 23:04:31 +0530

    basic homelayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <div className="bg-gray-50 h-screen max-w-[1920px]">Hello world</div>;
+  return (
+    <section className="bg-gray-50 h-screen max-w-[1920px] p-5">
+      <div>
+        <h2 className="font-montserrat font-bold text-sm mb-20">
+          Projects hub
+        </h2>
+      </div>
+      <div className="font-montserrat flex flex-col gap-3 justify-center items-center">
+        <div className="flex flex-col gap-1 text-center">
+          <h2 className="text-5xl font-bold">Projects Hub</h2>
+          <p className="text-xl">Revolutionary projects</p>
+        </div>
+        <p className="text-base font-dmsans font-bold">
+          We make price worthy projects. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Vel, tempora dicta. Corporis non ratione repelle
+        </p>
+        <button className="bg-secondary-400 rounded font-medium py-2 px-4 transition-all hover:scale-105 hover:bg-secondary-500">
+          Buy Now!
+        </button>
+      </div>
+    </section>
+  );
 }
