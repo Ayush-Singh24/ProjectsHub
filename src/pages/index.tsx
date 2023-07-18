a952c600108b57220d386abe065d55419e89ebbe
Ayush Singh <asayushsingh80@gmail.com>
2023-07-19 01:24:02 +0530

    adjusted homepage layout for different screen sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
 export default function Home() {
   return (
-    <section className="bg-gray-20 h-screen max-w-[1920px] p-5">
+    <section className="bg-gray-20 h-full md:h-[90%] max-w-[1550px] p-5 flex flex-col justify-center mx-auto">
       <div>
-        <h2 className="font-montserrat font-bold text-sm mb-20">
-          Projects hub
-        </h2>
+        <h2 className="font-montserrat font-bold text-sm">Projects hub</h2>
       </div>
-      <div className="font-montserrat flex flex-col gap-3 justify-center items-center">
-        <div className="flex flex-col gap-1 text-center">
-          <h2 className="text-5xl font-bold">Projects Hub</h2>
-          <p className="text-xl">Revolutionary projects</p>
+      <div className="font-montserrat flex flex-col gap-3 justify-center items-center h-3/4 my-auto lg:flex-row lg:justify-between">
+        <div className="flex flex-col gap-5 text-center max-w-3xl">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-5xl font-bold">Projects Hub</h2>
+            <p className="text-xl">Revolutionary projects</p>
+          </div>
+          <p className="text-base font-dmsans font-bold mb-5 text-left">
+            We make price worthy projects. Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Vel, tempora dicta. Corporis non
+            ratione repelle
+          </p>
+          <button className="self-center bg-secondary-400 rounded font-medium py-2 px-4 transition-all hover:scale-105 hover:bg-secondary-500">
+            Buy Now!
+          </button>
         </div>
-        <p className="text-base font-dmsans font-bold mb-5">
-          We make price worthy projects. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Vel, tempora dicta. Corporis non ratione repelle
-        </p>
-        <button className="bg-secondary-400 rounded font-medium py-2 px-4 transition-all hover:scale-105 hover:bg-secondary-500">
-          Buy Now!
-        </button>
-        <div>
-          <img src="/images/HomePageGraphic.png" alt="home" />
-        </div>
+        <img
+          src="/images/HomePageGraphic.png"
+          alt="home"
+          className="bg-gray-20 h-1/2 md:h-[1/2] lg:h-3/4 xl:h-[90%]"
+        />
       </div>
     </section>
   );
