6553ff2b986f06328e7fbc49690c988ba463fe0b
Ayush Singh <asayushsingh80@gmail.com>
2023-07-18 22:04:49 +0530

    tailwind configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return <div className="bg-gray-50 h-screen max-w-[1920px]">Hello world</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD58",
+        "secondart-500": "#FFC132",
       },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616 0%, #FFC837 100%)",
+        "mobile-home": "url()",
+      }),
     },
   },
   plugins: [],
-}
+};
