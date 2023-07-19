767532ee38aec1e53e4ef52b97f5a4962f94603e
Ayush Singh <asayushsingh80@gmail.com>
2023-07-19 19:59:36 +0530

    contact us button added in Header Component

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,7 +1,27 @@
 export default function Header() {
+  const bottomScrollPosition: ScrollToOptions = {
+    top: 100,
+    left: 0,
+    behavior: "smooth",
+  };
+
+  const goToBottom = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(event.target);
+    // window.scrollTo(bottomScrollPosition);
+  };
+
   return (
-    <div className="p-5 max-w-[1550px] w-full fixed mx-auto bg-gray-50">
-      <h2 className="font-montserrat font-bold text-sm">Projects hub</h2>
+    <div className="relative max-w-[1550px] w-full mx-auto">
+      <div className="p-5 max-w-[1550px] w-full fixed bg-gray-50 flex justify-between">
+        <h2 className="font-montserrat font-bold text-sm">Projects hub</h2>
+        <button
+          className="relative font-montserrat font-bold text-sm before:content-[''] before:h-1 before:absolute before:top-full before:left-0 before:w-0 
+          before:-z-10 before:transition-all before:bg-gray-500 before:rounded hover:before:w-full"
+          onClick={goToBottom}
+        >
+          Contact Us
+        </button>
+      </div>
     </div>
   );
 }
