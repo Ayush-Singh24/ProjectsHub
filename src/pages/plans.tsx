fd5d44b37bdcb338c096045a1b2757a4bb60049c
Ayush Singh <asayushsingh80@gmail.com>
2023-07-22 03:46:03 +0530

    made changes in page layout

--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -1,9 +1,9 @@
 export default function Plans() {
   return (
     <section className="h-full max-w-[1550px] mx-auto bg-gray-20 p-5 flex flex-col justify-center items-center">
-      <div className="flex justify-center h-3/4 gap-[10%] w-full">
-        <div className="bg-orange-500 basis-[30%]">hello</div>
-        <div className="bg-green-500 basis-[30%]">hello</div>
+      <div className="flex flex-col justify-center h-3/4 gap-[10%] w-full">
+        <div className="bg-orange-500 h-[50%]">hello</div>
+        <div className="bg-green-500 h-[50%]">hello</div>
       </div>
     </section>
   );
