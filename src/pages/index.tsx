a7171a7861cd251fab92ce8903aac350b34b5915
Ayush Singh <asayushsingh80@gmail.com>
2023-07-22 20:06:03 +0530

    adjusted z-index of psuedo elements in <h2> of hompage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,8 @@ export default function Home() {
           <div className="flex flex-col gap-2">
             <h2
               className="relative text-5xl font-bold after:content-['']
-               after:h-[105%] after:absolute after:top-0 after:p-1 after:-z-10 after:-right-6 after:w-[110%] after:transition-all after:bg-gray-20 bg-transparent after:animate-type-animation after:fill-mode-forwards 
+               after:h-[105%] after:absolute after:top-0 after:p-1 after:z-10 after:-right-6 after:w-[110%] after:transition-all after:bg-gray-20 bg-transparent after:animate-type-animation after:fill-mode-forwards 
+               before:z-20
             before:content-[''] before:bg-gray-500 before:w-2 before:h-[105%] before:absolute before:-right-6 before:animate-type-bar-animation before:fill-mode-forwards"
             >
               Projects Hub
