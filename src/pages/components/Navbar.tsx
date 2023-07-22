23b14003b8b91c44c2b52b4ba95bb9625a2c6d44
Ayush Singh <asayushsingh80@gmail.com>
2023-07-22 18:18:56 +0530

    added hamburger menu for navigation in small devices

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -24,8 +24,19 @@ export default function Navbar() {
     <div className="max-w-[1550px] w-full mx-auto">
       <div className="p-3 md:p-2 max-w-[1550px] w-full fixed bg-gray-100 flex justify-between items-center text-gray-500">
         <h2 className="font-montserrat font-bold text-sm">Projects hub</h2>
-        <div className="hidden md:block">
-          <ul className="flex gap-5">
+        <div className="">
+          <div className="md:hidden">
+            <input type="checkbox" className="hidden" name="nav-toggle" />
+            <label
+              htmlFor="nav-toggle"
+              className="h-10 w-10 rounded-[50%] flex justify-center items-center hover:bg-primary-300"
+            >
+              <span className="relative w-6 h-[3px] rounded bg-gray-500 inline-block transition-all after:rounded after:w-6 after:h-[3px] after:bg-gray-500 after:inline-block after:transition-all before:w-6 before:h-[3px] before:bg-gray-500 before:inline-block before:transition-all after:content-[''] after:absolute before:content-[''] before:rounded before:absolute after:left-0 before:left-0 after:-top-2 before:top-2 ">
+                &nbsp;
+              </span>
+            </label>
+          </div>
+          <ul className="hidden md:flex gap-5">
             <li
               className={`p-2 rounded transition-all ${
                 router.pathname === "/" ? NavLinkActiveState : ""
