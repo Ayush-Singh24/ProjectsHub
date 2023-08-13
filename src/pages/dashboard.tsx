0d7b90434a5300064103236c9dd6614214ad0fd7
Ayush Singh <asayushsingh80@gmail.com>
2023-08-13 22:16:07 +0530

    made dashboard layout 📈

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,10 @@
 export default function Dashboard() {
-  return <div>hello</div>;
+  return (
+    <section className="bg-gray-20 max-w-[1550px] flex flex-col justify-center items-center h-full mx-auto">
+      <div className="self-start w-full h-full p-6 mt-10 bg-orange-500 md:p-5">
+        <h2 className="text-2xl">Orders</h2>
+        <div className="bg-white rounded h-[95%]"></div>
+      </div>
+    </section>
+  );
 }
