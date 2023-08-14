a741f7d51872f64d1a08ed98f816bf45e4aca081
Ayush Singh <asayushsingh80@gmail.com>
2023-08-14 19:45:10 +0530

    improved efficiency of useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,21 +1,26 @@
 import { setIsAuth } from "@/redux/reducers/auth";
+import { RootState } from "@/redux/store";
 import { Service } from "@/service/service";
 import { ResponseStatus } from "@/utils/constants";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 export const useAuth = () => {
   const dispatch = useDispatch();
+  const { isAuth } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-    Service.verify(signal).then((response) => {
-      if (response.status === ResponseStatus.Ok) {
-        dispatch(setIsAuth(true));
-      } else {
-        dispatch(setIsAuth(false));
-      }
-    });
+    if (!isAuth) {
+      Service.verify(signal).then((response) => {
+        if (response.status === ResponseStatus.Ok) {
+          dispatch(setIsAuth(true));
+        } else {
+          dispatch(setIsAuth(false));
+        }
+      });
+    }
   }, []);
 };
